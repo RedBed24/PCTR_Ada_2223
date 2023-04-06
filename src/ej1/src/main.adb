f3a8e2e724845fe5ecdc7676af71f72fd62aeb01
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-04-06 13:59:36 +0200

    Add finished ej1

---
+++ src/ej1/src/main.adb
@@ -0,0 +1,11 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure main is
+
+begin
+   Put ("---- Inicio del programa main ----"); New_Line;
+   delay 5.0;
+   Put ("---- Fin del programa main ----"); New_Line;
+
+end main;
+
