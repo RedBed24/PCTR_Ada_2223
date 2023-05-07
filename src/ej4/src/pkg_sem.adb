4c7a91e5902276109fbee31d81eab8afebfcb283
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-05-07 13:22:42 +0200

    Terminada funcionalidad sem binario
    Cumplir con especificación (include/pkg_sem.ads):
    1. signal y wait son wrappers
    2. Definición signal y wait de sem_t

--- src/ej4/src/pkg_sem.adb
+++ src/ej4/src/pkg_sem.adb
@@ -2,25 +2,27 @@ package body pkg_sem is
 
    procedure wait (sem : in out sem_t) is
    begin
-      loop
-         select
-            when sem = 0 =>
-               sem := 1;
-               exit;
-         or
-            await sem;
-
-         end select;
-      end loop;
+      sem.wait;
    end wait;
 
    procedure signal (sem : in out sem_t) is
    begin
-      select
-         when sem = 1 =>
-            sem := 0;
-      end select;
+      sem.signal;
    end signal;
+
+   protected body sem_t is
+
+      entry wait when valor_sem > 0 is
+      begin
+         valor_sem := 0;
+      end wait;
+
+      procedure signal is
+      begin
+         valor_sem := 1;
+      end signal;
+
+   end sem_t;
 
 end pkg_sem;
 
