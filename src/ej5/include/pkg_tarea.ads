11ec7c2cb572f6665e22ca448619ef4cc6717904
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2023-05-08 13:54:53 +0200

    Add Makefile and specification of ej5

---
+++ src/ej5/include/pkg_tarea.ads
@@ -0,0 +1,10 @@
+With Ada.Real_Time; Use Ada.Real_Time;
+With Ada.Text_IO; Use Ada.Text_IO;
+
+package pkg_tarea is
+   procedure A;
+   procedure B;
+   procedure C;
+   procedure D;
+   procedure E;
+end pkg_tarea;
